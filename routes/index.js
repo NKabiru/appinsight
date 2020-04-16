f3a8d5b84390c83ffd9f85820ee4ac029d5c355d
NKabiru <njengakabiru@gmail.com>
2020-04-16 11:48:23 +0300

    Add axios and environment variables

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AppInsight' });
+  axios.defaults.baseURL = process.env.ONA_API_HOST;
+  axios.defaults.headers.common['Authorization'] = `Token ${process.env.ONA_API_KEY}`;
+
+  axios.get('user').then(response => {
+    console.log(response)
+    res.render('index', { title: 'AppInsight' });
+  }).catch(error => {
+    console.log(error)
+    res.render('index', { title: 'AppInsight' });
+  })
 });
 
 module.exports = router;
