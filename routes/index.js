cf8b4ad6f358e3388480c7a1611b78cd27dba02b
NKabiru <njengakabiru@gmail.com>
2020-04-17 15:39:39 +0300

    Update variables needed for Ona API

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios')
+const ona = require('../services/ona')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.defaults.baseURL = process.env.ONA_API_HOST;
-  axios.defaults.headers.common['Authorization'] = `Token ${process.env.ONA_API_KEY}`;
-
-  axios.get('user').then(response => {
-    console.log(response)
-    res.render('index', { title: 'AppInsight' });
-  }).catch(error => {
-    console.log(error)
-    res.render('index', { title: 'AppInsight' });
-  })
+  const data = ona.getForm();
+  console.log(data);
+  res.render('index', { title: 'AppInsight' });
 });
 
 module.exports = router;
