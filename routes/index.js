e9fb6e783ee45387e4948dd066f628e4171fa9f3
NKabiru <njengakabiru@gmail.com>
2020-05-01 10:48:46 +0300

    Add d3-array package to aggregate results from API

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const ona = require('../services/ona')
+const ona = require('../services/ona');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  console.log(await ona.getData());
+router.get('/',  async function(req, res, next) {
+  console.log(await ona.aggregate())
   res.render('index', { title: 'AppInsight' });
 });
 
--- services/ona.js
+++ services/ona.js
@@ -1,19 +1,29 @@
-const axios = require('axios');
-const formId = 501822;
+const axios = require('axios')
+const d3 = require('d3-array')
+const formId = 501822
 
-require('dotenv').config();
+require('dotenv').config()
 
-axios.defaults.baseURL = 'https://api.ona.io/api/v1';
-axios.defaults.headers.common['Authorization'] = `Token ${process.env.ONA_API_KEY}`;
+axios.defaults.baseURL = 'https://api.ona.io/api/v1'
+axios.defaults.headers.common['Authorization'] = `Token ${process.env.ONA_API_KEY}`
 
 async function getData() {
-    return await axios.get(`data/${formId}`)
+  return (await axios.get(`data/${formId}`)).data
 }
 
 async function aggregate() {
-     const data = await getData();
+    const data = await getData()
+
+    return {
+      browsers: d3.rollup(data, v => v.length, d => d['Primary_Browser']),
+      todos: d3.rollup(data, v => v.length, d => d['To_Do_Lists']),
+      officeSuites: d3.rollup(data, v => v.length, d => d['Office_Suite']),
+      cloudStorage: d3.rollup(data, v => v.length, d => d['Cloud_Storage']),
+      textEditors: d3.rollup(data, v => v.length, d => d['Preferred_Text_Editor']),
+      mailReaders: d3.rollup(data, v => v.length, d => d['Mail_Reader_Viewer'])
+    }
 }
 
 module.exports = {
-    getData
+  aggregate,
 }
