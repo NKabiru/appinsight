0d577cb017f96458cc822319b749471fdb6b2f72
NKabiru <njengakabiru@gmail.com>
2020-05-01 12:11:25 +0300

    Add the aggregrate results into the view object

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const ona = require('../services/ona');
 
 /* GET home page. */
 router.get('/',  async function(req, res, next) {
-  console.log(await ona.aggregate())
-  res.render('index', { title: 'AppInsight' });
+  const aggregates = await ona.aggregate()
+  res.render('index', { title: 'AppInsight' , aggregates});
 });
 
 module.exports = router;
